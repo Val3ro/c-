7d35ed8fc9aa56529885e62caeb2ab9ea99a5447
valeero <sergivsalvo@gmail.com>
2016-11-25 11:46:20 +0100

    Add files via upload

--- exercici5.c
+++ exercici5.c
@@ -10,7 +10,7 @@ typedef struct  {
 } Dades;
 
 char cadena[200];
-int opcio, i=0, j=0, num_atletes, k=0, l=0, m=0, n=0;
+int opcio=0, i=0, j=0, num_atletes, k=0, l=0, m=0, n=0;
 
 
 
@@ -25,90 +25,93 @@ void main() {
 		printf("\tOpcio: ");
 		scanf("%d", &opcio);
 
-	switch (opcio){
-		case 1:
-					printf("Atletes: ");
-					fflush(stdin);
-					scanf("%c", &cadena);
+		switch (opcio){
+				case 1:
+						printf("Atletes: ");
+						fflush(stdin);
+						gets(cadena);
 
-					printf("Informacio introduida amb exit!\n");
+						printf("Informacio introduida amb exit!\n");
 
-					//3$Francisco Araujo-12/03/2016-22.54-A$Arseny Belyakov-12/03/2016-25.03-A$Mike Smith-12/03/2016-12.45-B
+						//3$Francisco Araujo-12/03/2016-22.54-A$Arseny Belyakov-12/03/2016-25.03-A$Mike Smith-12/03/2016-12.45-B
 
-					//if (cadena[1] == '$') {
-						num_atletes = cadena[0] - '0';
-					/*}else {
-							if (cadena[1] == '0') {
-								num_atletes = 10;
+						//if (cadena[1] == '$') {
+							num_atletes = cadena[0] - '0';
+						/*}else {
+								if (cadena[1] == '0') {
+									num_atletes = 10;
+								}
+								if (cadena[1] != '0' && cadena[1] != '$'){
+									printf("Error, el màxim de atletes que es poden introduir son 10.\n");
+								}
 							}
-							if (cadena[1] != '0' && cadena[1] != '$'){
-								printf("Error, el màxim de atletes que es poden introduir son 10.\n");
-							}
-						}
-				*/
+						*/
 
-					for (n=0;n<num_atletes+1;n++){
-						while (i != 200) {
-							while (cadena[i] = '$'){
+						for (n=0;n<num_atletes+1;n++){
+							while (i != 200) {
+								while (cadena[i] == '$'){
+									i++;
+								}
+								while(cadena[i] != '-') {
+									dades[n].nomat[j] = cadena[i];
+									i++;
+									j++;
+								}
 								i++;
+								while (cadena[i] != '-') {
+									dades[n].dataat[k] = cadena[i];
+									i++;
+									k++;
+								}
+								i++;
+								while (cadena[i] != '-') {
+									dades[n].marcaat[l] = cadena[i];
+									i++;
+									l++;
+								}
+								i++;
+								while (cadena[i] != '$') {
+									dades[n].tipus[m] = cadena[i];
+									i++;
+									m++;
+								}
 							}
-						while(cadena[i] != '-') {
-							dades[n].nomat[j] = cadena[i];
-							i++;
-							j++;
 						}
-						i++;
-						while (cadena[i] != '-') {
-							dades[n].dataat[k] = cadena[i];
-							i++;
-							k++;
-						}
-						i++;
-						while (cadena[i] != '-') {
-							dades[n].marcaat[l] = cadena[i];
-							i++;
-							l++;
-						}
-						i++;
-						while (cadena[i] != '$') {
-							dades[n].tipus[m] = cadena[i];
-							i++;
-							m++;
-						}
-					}
-				}
-
 			break;
 		case 2:
 						printf("Prova A:\n");
 						for (n=0;n<num_atletes+1;n++){
-							if (dades[n].tipus[0] = 'A') {
+							if (dades[n].tipus[0] == 'A') {
 								printf("\t%s - %s\n", dades[n].nomat, dades[n].marcaat);
 							}
 						}
 						printf("Prova B:\n");
 						for (n=0;n<num_atletes+1;n++){
-							if (dades[n].tipus[0] = 'B') {
+							if (dades[n].tipus[0] == 'B') {
 								printf("\t%s - %s\n", dades[n].nomat, dades[n].marcaat);
 							}
 						}
 						printf("Prova C:\n");
 						for (n=0;n<num_atletes+1;n++){
-							if (dades[n].tipus[0] = 'C') {
+							if (dades[n].tipus[0] == 'C') {
 								printf("\t%s - %s\n", dades[n].nomat, dades[n].marcaat);
 							}
 						}
 						printf("Prova D:\n");
 						for (n=0;n<num_atletes+1;n++){
-							if (dades[n].tipus[0] = 'D') {
+							if (dades[n].tipus[0] == 'D') {
 								printf("\t%s - %s\n", dades[n].nomat, dades[n].marcaat);
 							}
 						}
 			break;
+		case 3:
+			break;
+		case 4:
+						printf("Adeuu!!!\n");
+			break;
 		default:
 						printf("La opcio introduida no es valida\n");
-			break;
-	}
-}
 
+		}
+	}
 }
